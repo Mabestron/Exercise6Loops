e13877da99eed23d7f55ef495f3e0d895e16f521
Mabestron <ivangamaliel555@hotmail.com>
2019-10-14 13:06:12 -0500

    Archivo de FOR terinado

--- src/com/Exercise6Loops/app/LoopFor.java
+++ src/com/Exercise6Loops/app/LoopFor.java
@@ -1,10 +1,56 @@
 package com.Exercise6Loops.app;
 
-public class LoopFor {
+public class LoopFor 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		//Constant Declaration
+		
+		//Variables Declaration
+		
+		//Object Construction
+		
+		//Create a serie 1-100 1 by 1
+		for (int i=1;i<=100;i ++) 
+		{
+		System.out.println(i);	
+		}
+		
+		//Create a serie 100-1 1 by 1
+		for (int i=100;i>=1;i--) 
+		{
+		System.out.println(i);		
+		}
+		
+		//Create a serie 1-100 2 by 2
+		for (int i=1;i<=100;i+=2) 
+		{
+		System.out.println(i);		
+		}
+		
+		//Create a serie 100-200 only in-par numbers
+		System.out.println("Display numbers 100-200 impairs");
+		for (int i=100;i<=200;i++) 
+		{
+			if(i%2==0)
+			{
+				continue;
+			}
+			
+		System.out.println(i);		
+		}
+		
+		//Create a serie 100-200 and stop in the middle
+				System.out.println("Display numbers 100-200 and stop in the middle");
+				for (int i=100;i<=200;i++) 
+				{
+					if(i==150)
+					{
+						break;
+					}
+					
+				System.out.println(i);		
+				}
 	}
-
 }
